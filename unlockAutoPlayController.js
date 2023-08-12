d5075eb938268bd3dcfab2fb8b93e8a76c55b0f7
Kevin Esaa <kevin.esaa@gmail.com>
2023-08-12 16:04:40 -0400

    feature: start video

--- unlockAutoPlayController.js
+++ unlockAutoPlayController.js
@@ -1,7 +1,29 @@
-console.log("aqui");
+const HIDE_STYLE_CLASS = "none";
 
+const VIDEO_SECTION_COMPONENT_ID = "unlock_auto_play_container";
+const VIDEO_PLAYER_COMPONENT_ID = "my-video-player";
 
+const KEYBOARD_CODES = {  BAR_SPACE:" "};
+let videoSection;
 let videoPlayer;
+
 function onLoadEventHandler() {
+    
+    videoSection = document.getElementById(VIDEO_SECTION_COMPONENT_ID);
+    videoPlayer = document.getElementById(VIDEO_PLAYER_COMPONENT_ID);
+    
+    window.addEventListener("keydown",onKeyButtonHandler);
+}
+
+function onKeyButtonHandler(event) {
+    
+    
+    if(event.key == KEYBOARD_CODES.BAR_SPACE)
+    {
+        videoPlayer.currentTime = 0;
+        videoPlayer.play();
+    }
 
 }
+
+window.onload = onLoadEventHandler;
