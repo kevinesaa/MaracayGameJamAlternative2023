5b8537e84b98813a212f437309eb9a6462108101
Pedro <pedrojoselr96@gmail.com>
2023-08-26 01:16:02 -0400

    some changes

--- js/AllSceneRaw.js
+++ js/AllSceneRaw.js
@@ -100,6 +100,7 @@ const ALL_SCENE_ARRAY = [
 
     /**separador*/
     /** Vamos a salir */
+    /** Scene 00001 */
     {
         id:"00001", // metada de guia
         name:"scene", // metada de guia
@@ -112,7 +113,7 @@ const ALL_SCENE_ARRAY = [
         substitles: [ 
             {character:"Narrador", text:"El clima se siente frío..."}, 
             {character:"Narrador", text:"¡Mira, una mariposa!"},
-            {character:"Narrador", text:"¿Alguna vez has pensado en cómo es la vida de las mariposa?"}
+            {character:"Narrador", text:"¿Alguna vez has pensado en cómo es la vida de las mariposas?"}
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -199,6 +200,7 @@ const ALL_SCENE_ARRAY = [
 
     /**separador*/
     /** Habla con la mantis */
+    /** Scene 00002 */
     {
         id:"00002", // metada de guia
         name:"intro", // metada de guia
@@ -209,10 +211,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0002/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"La mantis no te ha respondido...sera que no te entiende?"}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -260,6 +259,7 @@ const ALL_SCENE_ARRAY = [
 
     /**separador*/
     /** Recuestate en tu cama */
+    /** Scene 00003 */
     {
         id:"00003", // metada de guia
         name:"intro", // metada de guia
@@ -270,10 +270,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0003/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"Te quedaste dormido..."}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -331,10 +328,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0004/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"No se me ocurre que mas poner para avisar que aqui acaba el camino..."}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -382,6 +376,7 @@ const ALL_SCENE_ARRAY = [
 
     /**separador*/
     /** Si, y me deja pensando */
+    /** Scene 00005 */
     {
         id:"00005", // metada de guia
         name:"scene", // metada de guia
@@ -393,7 +388,7 @@ const ALL_SCENE_ARRAY = [
         /** los substítulos son opcionales */
         substitles: [ 
             {character:"Narrador", text:"A mi también. A lo largo de su vida sufren una serie de transformaciones complejas conocida como metamorfosis, en la que suceden cuatro fases conocidas como embrión, larva, pupa e imago."}, 
-            {character:"Narrador", text:"Nosotros los humanos también podemos pasar por metamorfosis a lo largo de nuestra vida a medida que vamos creciendo y nos exponemos a diferentes situaciones en la vida. Y dentro de nosotros mismos podemos llegar a evolucionar de maneras en que solo la naturaleza con su ecosistema nos puede reflejar a nostros mismos en otras especies. "}
+            {character:"Narrador", text:"Nosotros los humanos también podemos pasar por metamorfosis a lo largo de nuestra vida a medida que vamos creciendo y nos exponemos a diferentes situaciones en la vida. Y dentro de nosotros mismos podemos llegar a evolucionar de maneras en que solo la naturaleza con su ecosistema nos puede reflejar a nosotros mismos en otras especies. "}
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -443,43 +438,11 @@ const ALL_SCENE_ARRAY = [
             parentId:"00005", // metada de guia
             description: /* metada de guia */ "scene",
         },
-        children: [
-            {
-                /* este es el texto que ve el usuario para escoger esa acción*/ 
-                text: "Option 1" ,
-    
-                /** esta es la escena que se va a desarrollar si se escoge está opción*/
-                id:"00009", // metada de guia
-                name:"scene", // metada de guia
-                parentId:"00005", // metada de guia
-                description: /* metada de guia */ "scene",
-                
-            },
-            {
-                /* este es el texto que ve el usuario para escoger esa acción*/ 
-                text: "Option 2" ,
-    
-                /** esta es la escena que se va a desarrollar si se escoge está opción*/
-                id:"00010", // metada de guia
-                name:"scene", // metada de guia
-                parentId:"00005", // metada de guia
-                description: /* metada de guia */ "scene",
-            },
-            {
-                /* este es el texto que ve el usuario para escoger esa acción */ 
-                text: "Option 3" ,
-    
-                /** esta es la escena que se va a desarrollar si se escoge está opción*/
-                id:"00011", // metada de guia
-                name:"scene", // metada de guia
-                parentId:"00005", // metada de guia
-                description: /* metada de guia */ "scene",
-            }
-        ]
     },
     
     /**separador*/
     /** No realmente */
+    /** Scene 00006 */
     {
         id:"00006", // metada de guia
         name:"scena", // metada de guia
@@ -576,7 +539,7 @@ const ALL_SCENE_ARRAY = [
     },
 
     /**separador*/
-    /** Option 3 */
+    /** Scene 00007 */
     {
         id:"00007", // metada de guia
         name:"intro", // metada de guia
@@ -587,10 +550,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0004/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"Un Snorlax bloquea el camino!"}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -648,10 +608,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0004/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"Aqui acaba el camino..."}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -709,10 +666,181 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0004/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"Eso es todo amigos!"}, 
+        ],
+        sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
+        timmerStartAt: 6000, // 6 segundos después de iniciada la escena
+        timmerDuration: 4000, // 4 segundos el tiempo que dura el timmer
+        audioTracks: {
+            channel00:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel01:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel02:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+        
+            ],
+            channel03:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ]
+        } 
+    },
+
+    /**separador*/
+    /** Scene 00013 */
+    {
+        id:"00013", // metada de guia
+        name:"intro", // metada de guia
+        parentId:"00006", // metada de guia
+        description: /* metada de guia */ "se muestra cómo inicia la aventura. Presenta personajes y mundo.",
+        
+        mediaType: TYPES.IMAGEN,
+        mediaPath:"./res/scenes/scene00013/siuu.jpg",
+        /** los substítulos son opcionales */
+        substitles: [ 
+            {character:"Pedro", text:"EL BICHO SIUUUUUUUUUUUUUUUUUUUUUUUU"}, 
+        ],
+        sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
+        timmerStartAt: 6000, // 6 segundos después de iniciada la escena
+        timmerDuration: 4000, // 4 segundos el tiempo que dura el timmer
+        audioTracks: {
+            channel00:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel01:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel02:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+        
+            ],
+            channel03:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ]
+        } 
+    },
+
+    /**separador*/
+    /** Scene 00014 */
+    {
+        id:"00014", // metada de guia
+        name:"intro", // metada de guia
+        parentId:"00006", // metada de guia
+        description: /* metada de guia */ "se muestra cómo inicia la aventura. Presenta personajes y mundo.",
+        
+        mediaType: TYPES.IMAGEN,
+        mediaPath:"./res/scenes/scene0004/marco.png",
+        /** los substítulos son opcionales */
+        substitles: [ 
+            {character:"Pedro", text:"Oh no! Una mantis ha bloqueado tu camino!"},
+        ],
+        sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
+        timmerStartAt: 6000, // 6 segundos después de iniciada la escena
+        timmerDuration: 4000, // 4 segundos el tiempo que dura el timmer
+        audioTracks: {
+            channel00:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel01:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ],
+            channel02:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+        
+            ],
+            channel03:[{
+                    startAt:2000, // 2 segundos después de iniciada la escena para reproduccir el audio
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                },
+                {
+                    startAt:500, // 0.5 segundos después de finalizado el audio anterior
+                    trackPath: "./res/scena_00000/audio_white.ogg",
+                }
+            ]
+        } 
+    },
+
+    /**separador*/
+    /** Scene 00015 */
+    {
+        id:"00015", // metada de guia
+        name:"intro", // metada de guia
+        parentId:"00006", // metada de guia
+        description: /* metada de guia */ "se muestra cómo inicia la aventura. Presenta personajes y mundo.",
+        
+        mediaType: TYPES.IMAGEN,
+        mediaPath:"./res/scenes/scene0004/marco.png",
+        /** los substítulos son opcionales */
+        substitles: [ 
+            {character:"Pedro", text:"Hasta la vista baby"}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
@@ -770,10 +898,7 @@ const ALL_SCENE_ARRAY = [
         mediaPath:"./res/scenes/scene0004/marco.png",
         /** los substítulos son opcionales */
         substitles: [ 
-            {character:"Pedro", text:"Había una vez"}, 
-            {character:"Pedro", text:"En un lugar muy, muy lejano..."},
-            {character:"Narrador", text:"soy tu consciencia"}, 
-            {character:"Narrador", text:"esa voz en tu mente"}
+            {character:"Pedro", text:"Colorin colorado, este cuento se ha acabado"}, 
         ],
         sceneDuration: 10000, // 10 segundos, el tiempo que dura la escena
         timmerStartAt: 6000, // 6 segundos después de iniciada la escena
