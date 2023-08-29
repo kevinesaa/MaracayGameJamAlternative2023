9a5539a1a0b7b1df476430318141864032b471bb
Kevin Esaa <kevin.esaa@gmail.com>
2023-08-29 16:20:25 -0400

    refactor: better performece options section controller

--- js/GameplayOpctionsController.js
+++ js/GameplayOpctionsController.js
@@ -1,13 +1,14 @@
 
 const ID_GAMEPLAY_OPTIONS_COMPONENT =  "game_play_options_container";
 const ID_BASE_GAMEPLAY_OPTION_TEXT = "game_play_option_text_";
+const MAX_OPTIONS_BOX_SUPPORT = 3;
 
 class GameplayOptionsController {
 
 
     optionsSection;
     optionsSectionClassCss;
-    optionTexts = [0,0,0];
+    optionTexts = Array.from({length: MAX_OPTIONS_BOX_SUPPORT}).fill(0);
 
     constructor() {
 
@@ -16,7 +17,7 @@ class GameplayOptionsController {
     init() {
         this.optionsSection = document.getElementById(ID_GAMEPLAY_OPTIONS_COMPONENT);
         this.optionsSectionClassCss = this.optionsSection.style.display;
-        for(let i=0; i < 3; i++) {
+        for(let i = 0; i < this.optionTexts.length; i++) {
             const key = `${ID_BASE_GAMEPLAY_OPTION_TEXT}${i}`;
             this.optionTexts[i] = document.getElementById(key);
         }
@@ -24,14 +25,14 @@ class GameplayOptionsController {
     
     hideOptions() {
         this.optionsSection.style.display = HIDE_STYLE_CLASS;
-        for(let i=0; i < 3; i++) {
+        for(let i = 0; i < this.optionTexts.length; i++) {
             this.optionTexts[i].innerText = "";
         }
     }
 
     showOptions(optionsTextArray) {
         this.optionsSection.style.display = this.optionsSectionClassCss;
-        for(let i=0; i < 3; i++) {
+        for(let i = 0; i < this.optionTexts.length; i++) {
             const text = optionsTextArray[i];
             this.optionTexts[i].innerText = text;
         }
